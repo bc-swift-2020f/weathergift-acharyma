be2b84c190eb18aea4f1fedf4a1751e29e1c1630
Manogya Acharya <acharyma@bc.edu>
2020-10-10 14:46:38 -0400

    WeatherGift up to 6.3 completed

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Manogya Acharya on 10/10/20.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
