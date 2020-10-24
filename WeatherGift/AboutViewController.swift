431bfa958431bad99843ddb128d94a676e43d24b
Manogya Acharya <acharyma@bc.edu>
2020-10-24 16:19:27 -0400

    6.22 completed

---
+++ WeatherGift/AboutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutViewController.swift
+//  WeatherGift
+//
+//  Created by Manogya Acharya on 10/24/20.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
